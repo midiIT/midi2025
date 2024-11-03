c8c652f29941c1f2dc1a139650c1575ace990494
Dax0s <56531194+Dax0s@users.noreply.github.com>
2024-11-03 18:44:39 +0200

    1 domas  setup project base (#2)
    * Add tailwind
    
    * Add prettier
    
    * Add husky
    
    * Set vite port
    
    * Set up path resolution
    
    * Add react router

---
+++ src/components/GraphicalInterface/GraphicalInterface.tsx
@@ -0,0 +1,5 @@
+function GraphicalInterface() {
+  return <h1>Graphical interface</h1>;
+}
+
+export default GraphicalInterface;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '@/App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import GraphicalInterface from '@/components/GraphicalInterface/GraphicalInterface.tsx';
+import TerminalInterface from '@/components/TerminalInterface/TerminalInterface.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'gui',
+    element: <GraphicalInterface />,
+  },
+  {
+    path: 'cli',
+    element: <TerminalInterface />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
