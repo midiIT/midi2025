aa75c35dcc0d31c9763c74198fa57ea157d575c3
Benas <atomictacodev@gmail.com>
2024-11-08 15:30:37 +0200

    Added EventDisplay component which displays event info from JSON file

--- src/components/EventsPage/EventsPage.tsx
+++ src/components/EventsPage/EventsPage.tsx
@@ -1,5 +1,11 @@
+import EventDisplay from "@/components/EventsPage/EventDisplay.tsx";
+
 function EventsPage() {
-  return <h1>Events page</h1>;
+  return (
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <EventDisplay eventDate={"2025-01-01"}/>
+      </div>
+  );
 }
 
 export default EventsPage;
