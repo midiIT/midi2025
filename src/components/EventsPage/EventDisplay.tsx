2816899e555c1527af0a904f616dcea15deb9912
AtomicTacoDev <95696017+AtomicTacoDev@users.noreply.github.com>
2024-11-12 23:17:47 +0200

    Barebones events page (#12)
    * Added EventDisplay component which displays event info from JSON file
    
    * Added date picker which changes displayed event
    
    * changed weekday labels
    
    * week starts from monday
    
    * added event indication
    
    * removed console.logs
    
    ---------
    
    Co-authored-by: zhemepatis <rink.gab@gmail.com>

---
+++ src/components/EventsPage/EventDisplay.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import events from '@/events.json';
+
+interface EventDisplayProps {
+    eventDate: string;
+}
+
+const EventDisplay: React.FC<EventDisplayProps> = ({ eventDate }) => {
+    const eventInfo = events.find(event => event.date === eventDate);
+    
+    return (
+        <div className="max-w-sm w-full bg-gray-100 p-6 rounded-lg">
+            {eventInfo ? (
+                <div>
+                    <h2 className="text-xl font-semibold text-gray-800">{eventInfo.title}</h2>
+                    <p className="mt-2 text-gray-600">{eventInfo.description}</p>
+                    <p className="mt-4 text-sm text-gray-500">{eventInfo.date}</p>
+                </div>
+            ) : (
+                <div className="text-red-500">Error getting event.</div>
+            )}
+        </div>
+    );
+};
+
+export default EventDisplay;
--- src/components/EventsPage/EventsPage.tsx
+++ src/components/EventsPage/EventsPage.tsx
@@ -1,5 +1,17 @@
+import EventDisplay from "@/components/EventsPage/EventDisplay.tsx";
+import DatePicker from "@/components/EventsPage/DatePicker.tsx";
+import {useState} from "react";
+import events from "@/events.json";
+
 function EventsPage() {
-  return <h1>Events page</h1>;
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date(events[0].date));
+    
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <DatePicker onDatePicked={(date) => setSelectedDate(date)} />
+            <EventDisplay eventDate={selectedDate.toISOString().split('T')[0]} />
+        </div>
+    );
 }
 
 export default EventsPage;
