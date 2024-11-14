6c1c5e800f474b9a6c9989864ca858fd17d9bfd9
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-11-14 15:48:55 +0200

    Added initial terminal

--- src/components/TerminalInterface/TerminalInterface.tsx
+++ src/components/TerminalInterface/TerminalInterface.tsx
@@ -1,5 +1,28 @@
 function TerminalInterface() {
-  return <h1>Terminal interface</h1>;
+  return (
+    <section className="flex items-center justify-center w-full h-screen" >
+      <div className="bg-gray-800 w-[75%] h-[65%] rounded-2x1">
+        <div className="bg-gray-600 w-full h-[10%] rounded-2x1">
+          <button type="button"
+                  className="text-white bg-red-500 hover:bg-red-700 font-small py-0.5 px-2 mx-3 my-1
+                  rounded-full p-2.5 items-center float-right">X</button>
+        </div>
+        <div className="flex items-center justify-center h-[25%]">
+          <pre className="text-white text-center whitespace-pre leading-none mr-10">
+            {`
+           ____    ____ _____ ______   _____ 
+          |_   \\  /   _|_   _|_   _ \`.|_   _|
+            |   \\/   |   | |   | | \`. \\ | |  
+            | |\\  /| |   | |   | |  | | | |  
+           _| |_\\/_| |_ _| |_ _| |_.' /_| |_ 
+          |_____||_____|_____|______.'|_____|`}
+          </pre>
+        </div>
+        <br />
+        <p className="text-white text-center">{`=========================================================================================`}</p>
+      </div>
+    </section>
+  )
 }
 
 export default TerminalInterface;
