6a3533c1868401e453ef1020df96324c430f3d08
Kornelijus <kornbaublys@gmail.com>
2024-11-18 17:52:42 +0200

    delete styles.css, use tailwind

--- src/components/TwitchPage/TwitchPlayer.tsx
+++ src/components/TwitchPage/TwitchPlayer.tsx
@@ -1,11 +1,15 @@
 import { TwitchEmbed } from 'react-twitch-embed';
-import './twitchStyles.css';
 
 const TwitchPlayer = () => {
   return (
-    <div className="min-h-screen bg-black-900 text-white flex flex-col items-center justify-center">
-      <div className="w-full max-w-screen-lg">
-        <TwitchEmbed channel="midi_2024" muted className="twitch-player" />
+    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+      <div className="w-[400px] h-[300px] md:w-[1280px] md:h-[600px]">
+        <TwitchEmbed
+          channel="midi_2024"
+          muted
+          width="100%"
+          height="100%"
+        />
       </div>
     </div>
   );
