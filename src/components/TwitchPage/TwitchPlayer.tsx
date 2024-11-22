f16c1fb16d239c0cb5ffb30e8c033989c40bf704
Kornelijus <kornbaublys@gmail.com>
2024-11-22 17:37:29 +0200

    Twitch integracija (#27) (#34)
    * Sukurtas atskiras page Twitchui, kad nesimaišytų.
    
    * Sukurta Twitch integracija naudojantis react-twitch-embed.
    
    * Pritaikyta mobile versijai
    
    // NOTES //
    - Pahostinus ant networko meta errorą(https://prnt.sc/j5AMmmWa5oaE).
    - Ant local host viskas veikia, tik nežinau ar ant pačio domain'o veiks, tai reikėtų patikrinti.
    
    ---------
    
    Co-authored-by: Dax0s <dev@dklimavicius.com>

---
+++ src/components/TwitchPage/TwitchPlayer.tsx
@@ -0,0 +1,17 @@
+import { TwitchEmbed } from 'react-twitch-embed';
+
+const TwitchPlayer = () => {
+  return (
+    <div className="h-screen bg-black flex flex-col items-center justify-center">
+      <TwitchEmbed
+        channel="midi_2024"
+        muted
+        width="100%"
+        height="90%"
+        className="max-w-sm md:max-w-lg lg:max-w-full lg:p-8 w-full"
+      />
+    </div>
+  );
+};
+
+export default TwitchPlayer;
