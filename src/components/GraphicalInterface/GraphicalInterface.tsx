a0f5493b0f2d9ed3f9ffcd1e0502ea63399befe8
Karolisrib <Karolisrib@gmail.com>
2024-11-27 17:23:40 +0200

    Created minimal graphicla interface functionality. Widgets, applications that open a window

--- src/components/GraphicalInterface/GraphicalInterface.tsx
+++ src/components/GraphicalInterface/GraphicalInterface.tsx
@@ -1,15 +1,82 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
 import CRTDisplay from '@/components/CTRDisplay/CTRDisplay';
+import Application from '@/components/GraphicalInterface/Application'
+import ApplicationWindow from '@/components/GraphicalInterface/ApplicationWindow'
+import CountdownComponent from '../CountdownComponent/CountdownComponent';
+import DatePicker from '../EventsPage/DatePicker';
+import EventDisplay from '../EventsPage/EventDisplay';
+
+import RandomTerminalPng from '@/images/random_terminal.png'
+import RandomSomethingElsePng from '@/images/random_cat.jpeg'
+
+const TempTerminal: React.FC = () => {
+  return (
+    <p>Insert terminal as import later!!!</p>
+  )
+}
+const TempSomethingElse: React.FC = () => {
+  return (
+    <p>Insert something else here!</p>
+  )
+}
 
 const GraphicalInterface: React.FC = () => {
+
+  const [showWindow, setShowWindow] = useState<boolean>(false);
+  const [windowContent, setWindowContent] = useState<ReactNode>(null);
+
   return (
     <CRTDisplay
       initialPowerState={true}
       onPowerChange={isOn => console.log('Power state:', isOn)}
     >
-      <div className="space-y-2">
-        <p>GUI</p>
-        <p className="animate-pulse">█</p>
+      <div className='absolute w-1/2 left-1/4'>
+        <CountdownComponent/>
+      </div>
+      <div className='absolute w-1/2 left-1/2 top-1/2'>
+        <DatePicker
+          onDatePicked={ date =>{
+              setShowWindow(true);
+              setWindowContent(<EventDisplay eventDate={date.toISOString().split('T')[0]}/>);
+            }
+          }
+        />
+      </div>
+      <div className="inline-grid grid-cols-2 gap-4">
+        <Application
+          iconPath={RandomTerminalPng}
+          appText='Terminal'
+          windowContent = {TempTerminal}
+          onClick={ () => { 
+            setShowWindow(true) 
+            setWindowContent(<TempTerminal/>);
+          }}
+        />
+        <Application
+          iconPath={RandomSomethingElsePng}
+          appText='Something Elseeeeeeeeeeeeeeeeeeeeeeeeeee'
+          windowContent = {TempSomethingElse}
+          onClick={ () => { 
+            setShowWindow(true) 
+            setWindowContent(<TempSomethingElse/>);
+          }}
+        />
+        <Application
+          iconPath={RandomSomethingElsePng}
+          appText='Something Elseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+          windowContent = {TempSomethingElse}
+          onClick={ () => { 
+            setShowWindow(true) 
+            setWindowContent(<TempSomethingElse/>);
+          }}
+        />
+        {showWindow ? 
+          <ApplicationWindow
+            content={ windowContent }
+            onExit={ () => { setShowWindow(false)} }
+          /> 
+          : null
+        }
       </div>
     </CRTDisplay>
   );
