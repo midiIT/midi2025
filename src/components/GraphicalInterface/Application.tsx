9377332785d6b99e2fbb59a69ca789f55fcf77f1
Dax0s <dev@dklimavicius.com>
2024-11-29 17:00:01 +0200

    Run prettier

--- src/components/GraphicalInterface/Application.tsx
+++ src/components/GraphicalInterface/Application.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 
 interface ApplicationProps {
-	iconPath: string; 
-	appText: string;
-	windowContent: React.FC; 
-	onClick: (windowContent: React.FC) => void;
+  iconPath: string;
+  appText: string;
+  windowContent: React.FC;
+  onClick: (windowContent: React.FC) => void;
 }
-const Application: React.FC<ApplicationProps> = ({iconPath, appText, windowContent, onClick}) => {
-	return (
-		<div className='flex flex-col items-center'>
-			<button onDoubleClick={ () => {onClick(windowContent)} }>
-				<img
-					className='relative w-32 h-32 p-2 object-contain max-w-full'
-					src={iconPath}
-					alt="Application Icon"
-				/>
-				<p className="max-w-32 text-center mt-2 break-words">{appText}</p>
-			</button>
-		</div>
-	)
+const Application: React.FC<ApplicationProps> = ({
+  iconPath,
+  appText,
+  windowContent,
+  onClick,
+}) => {
+  return (
+    <div className="flex flex-col items-center">
+      <button
+        onDoubleClick={() => {
+          onClick(windowContent);
+        }}
+      >
+        <img
+          className="relative w-32 h-32 p-2 object-contain max-w-full"
+          src={iconPath}
+          alt="Application Icon"
+        />
+        <p className="max-w-32 text-center mt-2 break-words">{appText}</p>
+      </button>
+    </div>
+  );
 };
-export default Application
+export default Application;
--- src/components/GraphicalInterface/GraphicalInterface.tsx
+++ src/components/GraphicalInterface/GraphicalInterface.tsx
@@ -1,27 +1,22 @@
 import React, { ReactNode, useState } from 'react';
 import CRTDisplay from '@/components/CTRDisplay/CTRDisplay';
-import Application from '@/components/GraphicalInterface/Application'
-import ApplicationWindow from '@/components/GraphicalInterface/ApplicationWindow'
+import Application from '@/components/GraphicalInterface/Application';
+import ApplicationWindow from '@/components/GraphicalInterface/ApplicationWindow';
 import CountdownComponent from '../CountdownComponent/CountdownComponent';
 import DatePicker from '../EventsPage/DatePicker';
 import EventDisplay from '../EventsPage/EventDisplay';
 
-import RandomTerminalPng from '@/images/random_terminal.png'
-import RandomSomethingElsePng from '@/images/random_cat.jpeg'
+import RandomTerminalPng from '@/images/random_terminal.png';
+import RandomSomethingElsePng from '@/images/random_cat.jpeg';
 
 const TempTerminal: React.FC = () => {
-  return (
-    <p>Insert terminal as import later!!!</p>
-  )
-}
+  return <p>Insert terminal as import later!!!</p>;
+};
 const TempSomethingElse: React.FC = () => {
-  return (
-    <p>Insert something else here!</p>
-  )
-}
+  return <p>Insert something else here!</p>;
+};
 
 const GraphicalInterface: React.FC = () => {
-
   const [showWindow, setShowWindow] = useState<boolean>(false);
   const [windowContent, setWindowContent] = useState<ReactNode>(null);
 
@@ -30,53 +25,55 @@ const GraphicalInterface: React.FC = () => {
       initialPowerState={true}
       onPowerChange={isOn => console.log('Power state:', isOn)}
     >
-      <div className='absolute w-1/2 left-1/4'>
-        <CountdownComponent/>
+      <div className="absolute w-1/2 left-1/4">
+        <CountdownComponent />
       </div>
-      <div className='absolute w-1/2 left-1/2 top-1/2'>
+      <div className="absolute w-1/2 left-1/2 top-1/2">
         <DatePicker
-          onDatePicked={ date =>{
-              setShowWindow(true);
-              setWindowContent(<EventDisplay eventDate={date.toISOString().split('T')[0]}/>);
-            }
-          }
+          onDatePicked={date => {
+            setShowWindow(true);
+            setWindowContent(
+              <EventDisplay eventDate={date.toISOString().split('T')[0]} />,
+            );
+          }}
         />
       </div>
       <div className="inline-grid grid-cols-2 gap-4">
         <Application
           iconPath={RandomTerminalPng}
-          appText='Terminal'
-          windowContent = {TempTerminal}
-          onClick={ () => { 
-            setShowWindow(true) 
-            setWindowContent(<TempTerminal/>);
+          appText="Terminal"
+          windowContent={TempTerminal}
+          onClick={() => {
+            setShowWindow(true);
+            setWindowContent(<TempTerminal />);
           }}
         />
         <Application
           iconPath={RandomSomethingElsePng}
-          appText='Something Elseeeeeeeeeeeeeeeeeeeeeeeeeee'
-          windowContent = {TempSomethingElse}
-          onClick={ () => { 
-            setShowWindow(true) 
-            setWindowContent(<TempSomethingElse/>);
+          appText="Something Elseeeeeeeeeeeeeeeeeeeeeeeeeee"
+          windowContent={TempSomethingElse}
+          onClick={() => {
+            setShowWindow(true);
+            setWindowContent(<TempSomethingElse />);
           }}
         />
         <Application
           iconPath={RandomSomethingElsePng}
-          appText='Something Elseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-          windowContent = {TempSomethingElse}
-          onClick={ () => { 
-            setShowWindow(true) 
-            setWindowContent(<TempSomethingElse/>);
+          appText="Something Elseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+          windowContent={TempSomethingElse}
+          onClick={() => {
+            setShowWindow(true);
+            setWindowContent(<TempSomethingElse />);
           }}
         />
-        {showWindow ? 
+        {showWindow ? (
           <ApplicationWindow
-            content={ windowContent }
-            onExit={ () => { setShowWindow(false)} }
-          /> 
-          : null
-        }
+            content={windowContent}
+            onExit={() => {
+              setShowWindow(false);
+            }}
+          />
+        ) : null}
       </div>
     </CRTDisplay>
   );
