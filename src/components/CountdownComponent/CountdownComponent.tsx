123fdcd8162f7c24d14c624aee386bf649f6002a
Kornelijus <kornbaublys@gmail.com>
2024-12-10 18:13:18 +0200

    fixes

--- src/components/CountdownComponent/CountdownComponent.tsx
+++ src/components/CountdownComponent/CountdownComponent.tsx
@@ -43,7 +43,7 @@ const CountdownComponent: React.FC = () => {
 
   // with little tailwind css
   return (
-    <div className=" bg-blue-500 text-white p-6 rounded-lg flex flex-col items-center">
+    <div className=" bg-blue-500 text-white p-6 rounded-lg flex flex-col items-center sm:w-full sm:h-full">
       <h1 className="text-2xl font-bold mb-4">Time Left</h1>
       <p className="text-lg font-mono bg-blue-800 p-4 rounded-md">
         {timeLeft.days} <span className="font-semibold">Days</span>,{' '}
--- src/components/MobileInterface/MobileInterface.tsx
+++ src/components/MobileInterface/MobileInterface.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CountdownComponent from '../CountdownComponent/CountdownComponent';
 import DatePicker from '../EventsPage/DatePicker';
 import EventDisplay from '../EventsPage/EventDisplay';
 import SwipeablePages from './SwipeablePages';
-import PreventPullToRefresh from '@/components/MobileInterface/PreventPullToRefresh.tsx';
 
 interface PhoneDisplayProps {
   className?: string;
@@ -17,6 +16,28 @@ const MobileInterface: React.FC<PhoneDisplayProps> = ({ className = '' }) => {
 
   const togglePower = () => setPowerOn(!powerOn);
 
+  useEffect(() => {
+    // Apply styles to the html and body
+    document.documentElement.style.height = '100%';
+    document.body.style.height = '100%';
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';
+    document.body.style.overscrollBehavior = 'none';
+    document.body.style.backgroundColor = '#111827';
+    document.body.style.overflow = 'auto';
+
+    return () => {
+      // Cleanup styles when the component is unmounted
+      document.documentElement.style.height = '';
+      document.body.style.height = '';
+      document.body.style.margin = '';
+      document.body.style.padding = '';
+      document.body.style.overscrollBehavior = '';
+      document.body.style.backgroundColor = '';
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   const pages = [
     <div className="text-center text-green-400 text-base">
       <p>Home Page</p>
@@ -24,7 +45,7 @@ const MobileInterface: React.FC<PhoneDisplayProps> = ({ className = '' }) => {
     <div className="w-full h-full flex">
       <CountdownComponent />
     </div>,
-    <div className="w-full h-full flex">
+    <div className="w-full h-full flex overflow-y-auto">
       <DatePicker
         onDatePicked={date => {
           setSelectedDate(date.toISOString().split('T')[0]);
@@ -35,90 +56,88 @@ const MobileInterface: React.FC<PhoneDisplayProps> = ({ className = '' }) => {
   ];
 
   return (
-    <PreventPullToRefresh>
+    <div
+      className={`flex items-center justify-center h-screen w-screen bg-gray-900 overflow-hidden  ${className}`}
+    >
+      {/* Phone box */}
       <div
-        className={`flex items-center justify-center h-screen w-screen bg-gray-900 overflow-hidden ${className}`}
-      >
-        {/* Phone box */}
-        <div
-          className="relative w-[100vw] h-[100%] landscape:w-[100%] landscape:h-[90%]
+        className="relative w-[100vw] h-[100%] landscape:w-[100%] landscape:h-[90%]
                    flex flex-row justify-between items-center 
                    bg-gradient-to-br from-gray-800 to-gray-900 rounded-[2rem] shadow-2xl border-[10px]
                    border-gray-600 p-4"
-        >
-          {/* Speaker Slot */}
-          <div
-            className="portrait:absolute top-2 w-[25%] h-2 bg-gray-600 rounded-full left-[37%]
+      >
+        {/* Speaker Slot */}
+        <div
+          className="portrait:absolute top-2 w-[25%] h-2 bg-gray-600 rounded-full left-[37%]
                      landscape:rotate-[270deg] landscape:w-[10%] landscape:top-1/2 landscape:left-1/2
                      landscape:transform landscape:-translate-x-1/2"
-          ></div>
+        ></div>
 
-          {/* MIDI */}
-          <div
-            className="absolute top-6 left-0 w-full text-center text-white font-bold text-xl tracking-wider
+        {/* MIDI */}
+        <div
+          className="absolute top-6 left-0 w-full text-center text-white font-bold text-xl tracking-wider
                      landscape:top-1/2 landscape:transform landscape:-translate-x-1/2 landscape:-translate-y-1/2 
                      landscape:rotate-[270deg] landscape:origin-center landscape:left-10"
-          >
-            MIDI 2025
-          </div>
+        >
+          MIDI 2025
+        </div>
 
-          {/* Screen Area */}
-          <div
-            className={`absolute inset-0 h-[83vh] w-[90vw] left-[2.5vw] top-[7vh] bg-black rounded-[1rem] border-4 border-gray-600
-                     overflow-hidden transition-opacity duration-500 landscape:h-[85%] landscape:w-[55%] shadow-inner
-                     landscape:left-[8%] landscape:top-[7%]  ${powerOn ? 'opacity-100' : 'opacity-50'}`}
-          >
-            {powerOn && (
-              <div className="relative h-full w-full flex">
-                <SwipeablePages
-                  pages={pages.map(page => (
-                    <div className="absolute inset-0 h-full w-full flex flex-col overflow-y-auto scrollbar-hide">
-                      {page}
-                    </div>
-                  ))}
-                  currentPage={currentPage}
-                  setCurrentPage={setCurrentPage}
-                />
-
-                {/* Event Pop-Up */}
-                {showEventInfo && selectedDate && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 rounded-[1rem]">
-                    <div className="relative bg-gray-800 rounded-lg shadow-lg p-6 w-[90%] h-[80%] overflow-y-auto">
-                      <button
-                        onClick={() => setShowEventInfo(false)}
-                        className="absolute top-2 right-2 w-8 h-8 bg-red-600 text-white rounded-full font-bold flex items-center justify-center"
-                      >
-                        ✕
-                      </button>
-                      <EventDisplay eventDate={selectedDate} />
-                    </div>
+        {/* Screen Area */}
+        <div
+          className={`absolute h-[82vh] w-[90vw] left-[2.5vw] top-[7vh] bg-black rounded-[1rem] border-4 border-gray-600
+                     overflow-hidden transition-opacity duration-500 landscape:h-[80vh] landscape:w-[80vw] shadow-inner
+                     landscape:left-[8vw] landscape:top-[2.5vh]  ${powerOn ? 'opacity-100' : 'opacity-50'}`}
+        >
+          {powerOn && (
+            <div className="relative h-full w-full overflow-y-auto">
+              <SwipeablePages
+                pages={pages.map(page => (
+                  <div className="absolute inset-0 h-full w-full flex flex-col overflow-y-auto scrollbar-hide">
+                    {page}
                   </div>
-                )}
-              </div>
-            )}
-          </div>
+                ))}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
 
-          {/* Controls */}
-          <div
-            className={`absolute portrait:bottom-[1vh] portrait:w-full flex flex-col items-center left-0 portrait:space-y-4 
-                     landscape:absolute landscape:left-[80%] landscape:top-1/2 
-                     landscape:transform landscape:-translate-x-1/2 landscape:-translate-y-1/2
-                     landscape:rotate-[270deg] landscape:space-y-6`}
-          >
-            {/* Power Button */}
-            <button
-              onClick={togglePower}
-              className={`w-14 h-14 rounded-full bg-gray-500 text-white font-bold hover:bg-gray-400
+              {/* Event Pop-Up */}
+              {showEventInfo && selectedDate && (
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 rounded-[1rem]">
+                  <div className="relative bg-gray-800 rounded-lg shadow-lg p-6 w-[90%] h-[80%] overflow-y-auto">
+                    <button
+                      onClick={() => setShowEventInfo(false)}
+                      className="absolute top-2 right-2 w-8 h-8 bg-red-600 text-white rounded-full font-bold flex items-center justify-center"
+                    >
+                      ✕
+                    </button>
+                    <EventDisplay eventDate={selectedDate} />
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+
+        {/* Controls */}
+        <div
+          className={`absolute portrait:bottom-[1vh] portrait:w-full flex flex-col items-center left-0 portrait:space-y-4 
+               landscape:absolute landscape:bottom-[35vh] landscape:-right-[88vw] 
+               landscape:transform landscape:translate-y-0 landscape:translate-x-0 
+               landscape:rotate-[270deg] landscape:space-y-6`}
+        >
+          {/* Power Button */}
+          <button
+            onClick={togglePower}
+            className={`w-14 h-14 rounded-full bg-gray-500 text-white font-bold hover:bg-gray-400
                        active:bg-gray-600 shadow-xl ${
                          powerOn
                            ? 'ring-4 ring-green-500'
                            : 'ring-4 ring-red-500'
                        }`}
-            />
-          </div>
+          />
         </div>
       </div>
-    </PreventPullToRefresh>
+    </div>
   );
 };
 
