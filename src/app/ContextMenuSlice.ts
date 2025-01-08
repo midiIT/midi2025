f4a46bafc49ec143d58732437df432096d1d100e
Dax0s <dev@dklimavicius.com>
2025-01-08 18:44:01 +0200

    Fix memoization

--- src/app/ContextMenuSlice.ts
+++ src/app/ContextMenuSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '@/app/Store.ts';
 
 interface ContextMenuSlice {
@@ -46,13 +46,20 @@ export const { openContextMenu, closeContextMenu } = contextMenuSlice.actions;
 
 export const selectContextMenuOpen = (state: RootState) =>
   state.contextMenu.open;
-export const selectContextMenuData = (state: RootState) => {
-  return {
-    x: state.contextMenu.x,
-    y: state.contextMenu.y,
-    title: state.contextMenu.title,
-    owner: state.contextMenu.owner,
-  };
-};
+
+const selectX = (state: RootState) => state.contextMenu.x;
+const selectY = (state: RootState) => state.contextMenu.y;
+const selectTitle = (state: RootState) => state.contextMenu.title;
+const selectOwner = (state: RootState) => state.contextMenu.owner;
+
+export const selectContextMenuData = createSelector(
+  [selectX, selectY, selectTitle, selectOwner],
+  (x, y, title, owner) => ({
+    x,
+    y,
+    title,
+    owner,
+  }),
+);
 
 export default contextMenuSlice.reducer;
