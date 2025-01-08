263d7427164e08eb01baa8052647e59f2cfb4948
Dax0s <dev@dklimavicius.com>
2025-01-08 18:30:56 +0200

    Add description for application data type and remove unnecessary applications

--- src/components/GraphicalInterface/ApplicationList.tsx
+++ src/components/GraphicalInterface/ApplicationList.tsx
@@ -1,51 +1,11 @@
 import { Application } from '@/components/GraphicalInterface/types.ts';
-import React from 'react';
-import randomTerminalPng from '@/images/random_terminal.png';
-import randomSomethingElsePng from '@/images/random_cat.jpeg';
 import calendarPng from '@/images/calendar.png';
 import EventDisplay from '@/components/EventsPage/EventDisplay.tsx';
 import { DEFAULT_Z_INDEX } from '@/components/GraphicalInterface/consts.ts';
 import TeamPage from '@/components/TeamPage/TeamPage.tsx';
 import teamAppIcon from '@/images/TeamAppIcon.jpg';
 
-const TempTerminal: React.FC = () => {
-  return <p>Insert terminal as import later!!!</p>;
-};
-const TempSomethingElse: React.FC = () => {
-  return <p>Insert something else here!</p>;
-};
-
 const applications: Application[] = [
-  {
-    windowContent: <TempTerminal />,
-    hidden: false,
-    data: {
-      title: 'Terminal',
-      iconPath: randomTerminalPng,
-      minimized: false,
-      zIndex: DEFAULT_Z_INDEX,
-    },
-  },
-  {
-    windowContent: <TempSomethingElse />,
-    hidden: false,
-    data: {
-      title: 'Something 1',
-      iconPath: randomSomethingElsePng,
-      minimized: false,
-      zIndex: DEFAULT_Z_INDEX,
-    },
-  },
-  {
-    windowContent: <TempSomethingElse />,
-    hidden: false,
-    data: {
-      title: 'Something 2',
-      iconPath: randomSomethingElsePng,
-      minimized: false,
-      zIndex: DEFAULT_Z_INDEX,
-    },
-  },
   {
     windowContent: <EventDisplay />,
     hidden: true,
--- src/components/GraphicalInterface/types.ts
+++ src/components/GraphicalInterface/types.ts
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react';
 
+/**
+ * @minimized whether the application should be shown or minimized
+ * @title application title
+ * @zIndex application zIndex, used for correct stacking of applications
+ * @iconPath application icon path
+ */
 export interface ApplicationData {
   minimized: boolean;
   title: string;
@@ -7,6 +13,11 @@ export interface ApplicationData {
   iconPath: string;
 }
 
+/**
+ * @data data for the application
+ * @windowContent what to show when application is open
+ * @hidden whether the application icon should be on the desktop
+ */
 export interface Application {
   data: ApplicationData;
   windowContent: ReactNode;
