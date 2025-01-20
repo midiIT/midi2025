e9e310ec231425d99382139e6166817f92645dde
ramuneri <ramune.riau@gmail.com>
2025-01-20 10:41:35 +0200

    Events with scroll bar

--- src/components/EventsPage/EventDisplay.tsx
+++ src/components/EventsPage/EventDisplay.tsx
@@ -9,29 +9,30 @@ const EventDisplay: React.FC<EventDisplayProps> = ({ eventDate }) => {
   const eventsForDate = events.filter(event => event.date === eventDate);
 
   return (
-    <div className="w-100 h-80 bg-black p-6 rounded-lg mt-4">
+    <div
+      className="w-full bg-black p-6 rounded-lg mt-4 flex flex-col overflow-y-auto"
+      style={{ maxHeight: '80vh' }}
+    >
       {eventsForDate.length > 0 ? (
         eventsForDate.map((event, index) => (
           <div key={index} className="mb-4">
             <h2 className="text-xl font-semibold" style={{ color: '#0175B4' }}>
-              {' '}
-              {event.title}{' '}
+              {event.title}
             </h2>
 
-            {/* <p className="mt-2" style={{ color: '#0175B4' }}> {event.description}</p> */}
-            <div key={index} className="text-gray-400 mt-2">
-              {event.description.split('\n').map((line, i) => (
-                <React.Fragment key={i}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
+            <div
+              className="text-gray-400 mt-2"
+              style={{ whiteSpace: 'pre-wrap' }}
+            >
+              {event.description}
             </div>
 
             <p className="mt-5" style={{ color: '#0175B4' }}>
               {event.date}
             </p>
-            {index < eventsForDate.length - 1 && <hr className="my-2" />}
+            {index < eventsForDate.length - 1 && (
+              <hr className="my-2 border-gray-600" />
+            )}
           </div>
         ))
       ) : (
