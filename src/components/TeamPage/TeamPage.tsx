aa54505db3eeeff5894a35ce11630ddc6924dd09
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2025-01-30 21:45:45 +0200

    Fixing team page

--- src/components/TeamPage/TeamPage.tsx
+++ src/components/TeamPage/TeamPage.tsx
@@ -4,8 +4,7 @@ import TeamContent from './TeamContent';
 function TeamPage() {
   return (
     <div className="relative min-h-screen w-full">
-      <div className="absolute inset-0 bg-midi-blue bg-cover bg-center bg-no-repeat"></div>
-      <nav className="bg-black text-white py-4 px-6 shadow-md fixed w-full top-0 z-20">
+      <nav className="bg-black text-white py-4 px-6 shadow-md w-full z-20">
         <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center">
           MIDI komanda
         </h1>
