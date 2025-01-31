74aaf78edc615af0c100871db759bfaa7c565101
Dax0s <dev@dklimavicius.com>
2025-01-31 16:45:31 +0200

    Fix events page error

--- src/components/EventsPage/EventsPage.tsx
+++ src/components/EventsPage/EventsPage.tsx
@@ -1,18 +1,10 @@
 import EventDisplay from '@/components/EventsPage/EventDisplay.tsx';
 import DatePicker from '@/components/EventsPage/DatePicker.tsx';
-import { useAppDispatch } from '@/app/hooks.ts';
-import { setEventDate } from '@/app/ApplicationsSlice.ts';
 
 function EventsPage() {
-  const dispatch = useAppDispatch();
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <DatePicker
-        onDatePicked={date =>
-          dispatch(setEventDate(date.toISOString().split('T')[0]))
-        }
-      />
+      <DatePicker />
       <EventDisplay />
     </div>
   );
