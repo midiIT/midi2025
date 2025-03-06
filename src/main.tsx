fd16e91147e06d38e6389992b83b6ae5e8fc8f35
Dax0s <dev@dklimavicius.com>
2025-03-06 19:12:55 +0200

    Fix h1 tags

--- src/main.tsx
+++ src/main.tsx
@@ -80,13 +80,13 @@ function addTracking(routes: RouteObject[]): RouteObject[] {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>MIDI įmonių mugė</h1>
-    <h1>MIDI imoniu muge</h1>
-    <h1>MIDI ĮMONIŲ MUGĖ</h1>
-    <h1>MIDI IMONIU MUGE</h1>
-    <h1>MIDI</h1>
-    <h1>imoniu muge</h1>
-    <h1>įmonių mugė</h1>
+    <h1 style={{ display: 'none' }}>MIDI įmonių mugė</h1>
+    <h1 style={{ display: 'none' }}>MIDI imoniu muge</h1>
+    <h1 style={{ display: 'none' }}>MIDI ĮMONIŲ MUGĖ</h1>
+    <h1 style={{ display: 'none' }}>MIDI IMONIU MUGE</h1>
+    <h1 style={{ display: 'none' }}>MIDI</h1>
+    <h1 style={{ display: 'none' }}>imoniu muge</h1>
+    <h1 style={{ display: 'none' }}>įmonių mugė</h1>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
